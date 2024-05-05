ffaf1ba231b4c114f8c58db95d59107cd34efc8d
Frank Lange <langefrq@gmx.de>
2024-05-05 20:31:11 +0200

    add scanln impl
    foo

---
+++ scanln.go
@@ -0,0 +1,50 @@
+package scanln
+
+import (
+	"fmt"
+	"os"
+)
+
+type Scanln struct {
+	C chan string
+	q chan bool
+}
+
+func NewScanln() *Scanln {
+	s := &Scanln{make(chan string), make(chan bool, 1)}
+	go s.runForever()
+
+	return s
+}
+
+func (s *Scanln) Stop() {
+	s.q <- true
+
+	file := os.NewFile(0, "stdin")
+	_, err := file.Write([]byte(``))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *Scanln) runOne() string {
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		panic(err)
+	}
+
+	return input
+}
+
+func (s *Scanln) runForever() {
+	for {
+		input := s.runOne()
+		select {
+		case <-s.q:
+			return
+		default:
+			s.C <- input
+		}
+	}
+}
